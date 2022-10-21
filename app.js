7b1034278e4dab3b22715bc41e8e55b77f9328af
mkk100 <112575297+mkk100@users.noreply.github.com>
2022-10-20 21:30:11 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const form = document.querySelector('#searchForm');
+form.addEventListener('submit', async function(e){
+    e.preventDefault();
+    let searchTerm = form.elements.query.value
+    let res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)
+    makeImages(res.data)
+    form.elements.query.value = ''
+})
+const makeImages = (shows) =>{
+    for(let result of shows){
+        if(result.show.image){
+        const img = document.createElement('IMG');
+        img.src = result.show.image.medium
+        document.body.append(img)
+        }
+    }
+}
